6a3a41503a289cf64e218f0f6521ed4506740ceb
Abdelrahman Mohamed Ali <102557725+AbdoAlshoki2@users.noreply.github.com>
2025-04-14 00:24:35 +0200

    Handled Scalar values in pow function
    Modified the pow function to work with Scalar instances as well, and cleaned the code with more indentations.

--- s_grad/Scalar.py
+++ s_grad/Scalar.py
@@ -1,77 +1,83 @@
+import math
+
 class Scalar:
     def __init__(self, data, _children = ()):
-      self.data = data
-      self._prev = set(_children)
+        self.data = data
+        self._prev = set(_children)
 
-      self._backward = lambda: None
-      self.grad = 0.0
+        self._backward = lambda: None
+        self.grad = 0.0
 
 
     def __add__(self,other):
-      other = other if isinstance(other, Scalar) else Scalar(other)
-      out = Scalar(self.data + other.data , (self, other))
-      def _backward():
-        self.grad += out.grad
-        other.grad += out.grad
-      out._backward = _backward
-      return out
+        other = other if isinstance(other, Scalar) else Scalar(other)
+        out = Scalar(self.data + other.data , (self, other))
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
 
     def __mul__(self, other):
-      other = other if isinstance(other, Scalar) else Scalar(other)
-      out = Scalar(self.data * other.data , (self, other))
-      def _backward():
-        self.grad += other.data * out.grad
-        other.grad += self.data * out.grad
-      out._backward = _backward
-      return out
+        other = other if isinstance(other, Scalar) else Scalar(other)
+        out = Scalar(self.data * other.data , (self, other))
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
 
-    def __pow__(self , other):
-      assert isinstance(other, (int, float)), "only supporting int/float powers for now"
-      out = Scalar(self.data**other , (self,), f'**{other}')
-      def _backward():
-        self.grad += (other * self.data**(other-1)) * out.grad
-      out._backward = _backward
-      return out
+    def __pow__(self, other):
+        other = other if isinstance(other, Scalar) else Value(other)
+        out = Scalar(self.data**other.data, (self, other))
+        if self.data <= 0: # not implemented for negative bases
+          raise ValueError("Cannot compute gradients for negative bases raised to variable powers")
+
+        def _backward():
+            self.grad += other.data * (self.data**(other.data-1)) * out.grad # power rule (n) * p ** n-1
+            other.grad += (self.data**other.data) * math.log(self.data) * out.grad # ln(x) * x ** n
+        
+        out._backward = _backward
+        return out
 
     def __truediv__(self, other):
-      return self * other**-1
+        return self * other**-1
 
     def __neg__(self):
-      return self * -1
+        return self * -1
 
     def __sub__(self, other):
-      return self + (-other)
+        return self + (-other)
 
     def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
 
-      topo = []
-      visited = set()
-      def build_topo(v):
-          if v not in visited:
-              visited.add(v)
-              for child in v._prev:
-                  build_topo(child)
-              topo.append(v)
-      build_topo(self)
-
-      self.grad = 1
-      for v in reversed(topo):
-          v._backward()
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
 
     def __rmul__(self, other):
-      return self * other
+        return self * other
 
     def __radd(self , other):
-      return self + other
+        return self + other
         
     def __rsub__(self, other): 
-      return other + (-self)
+        return other + (-self)
 
     def __rtruediv__(self, other):
-      return other * self**-1
+        return other * self**-1
 
     def __repr__(self):
-      return f"Scalar(data={self.data} , grad={self.grad})"
+        return f"Scalar(data={self.data} , grad={self.grad})"
     
 
 
