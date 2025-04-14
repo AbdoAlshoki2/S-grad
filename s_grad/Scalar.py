20a66e9738bc7b69560bb01f60460b18e9a83ab1
Abdelrahman Mohamed Ali <102557725+AbdoAlshoki2@users.noreply.github.com>
2025-04-14 14:54:02 +0200

    Added sigmoid and tanh functions
    Added sigmoid and tanh functions and also Edited some typos in functions like radd and exp backward.

--- s_grad/Scalar.py
+++ s_grad/Scalar.py
@@ -28,7 +28,7 @@ class Scalar:
         return out
 
     def __pow__(self, other):
-        other = other if isinstance(other, Scalar) else Value(other)
+        other = other if isinstance(other, Scalar) else Scalar(other)
         out = Scalar(self.data**other.data, (self, other))
         if self.data <= 0: # not implemented for negative bases
           raise ValueError("Cannot compute gradients for negative bases raised to variable powers")
@@ -40,37 +40,6 @@ class Scalar:
         out._backward = _backward
         return out
     
-    def sin(self):
-        out = Scalar(math.sin(self.data) , (self,))
-        def _backward():
-            self.grad += math.cos(self.data) * out.grad
-        out._backward = _backward
-        return out
-
-    def exp(self):
-        out = Scalar(math.exp(self.data) , (self,))
-        def _backwward():
-            self.grad += out.data * out.grad
-        out._backward = _backwward
-        return out
-
-    def log(self):
-        if self.data <= 0: # not implemented for negative bases
-          raise ValueError("Cannot compute log of non-positive value")
-        
-        out = Scalar(math.log(self.data) , (self,))
-        def _backward():
-            self.grad += (1.0/self.data) * out.grad 
-        out._backward = _backward
-        return out     
-
-    def relu(self):
-      out = Scalar(max(self.data , 0) , (self,))
-      def _backward():
-        self.grad += (out.data > 0) * out.grad
-      out._backward = _backward
-
-      return out
 
     def __truediv__(self, other):
         return self * other**-1
@@ -99,7 +68,7 @@ class Scalar:
     def __rmul__(self, other):
         return self * other
 
-    def __radd(self , other):
+    def __radd__(self , other):
         return self + other
         
     def __rsub__(self, other): 
@@ -110,6 +79,59 @@ class Scalar:
 
     def __repr__(self):
         return f"Scalar(data={self.data} , grad={self.grad})"
-    
 
+    def sin(self):
+        out = Scalar(math.sin(self.data) , (self,))
+        def _backward():
+            self.grad += math.cos(self.data) * out.grad
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        out = Scalar(math.exp(self.data) , (self,))
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+
+    def log(self):
+        if self.data <= 0: # not implemented for negative bases
+          raise ValueError("Cannot compute log of non-positive value")
+        
+        out = Scalar(math.log(self.data) , (self,))
+        def _backward():
+            self.grad += (1.0/self.data) * out.grad 
+        out._backward = _backward
+        return out     
+
+    def relu(self):
+        out = Scalar(max(self.data , 0) , (self,))
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+        return out
+
+    def sigmoid(self):
+
+        neg_self = self * -1
+        exp_term = neg_self.exp()
+        one = Scalar(1.0)
+        denom = one + exp_term
+        out = one / denom
+        def _backward():
+            self.grad += (out.data * (1 - out.data)) * out.grad
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+
+        pos_exp = (self * 2).exp()
+        neg_exp = (self * -2).exp()
+        num = pos_exp - neg_exp
+        denom = pos_exp + neg_exp
+        out = num / denom 
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
+        out._backward = _backward
+        return out
 
